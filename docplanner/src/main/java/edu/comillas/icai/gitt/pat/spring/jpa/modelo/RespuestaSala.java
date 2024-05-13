7130f3e6517f52996d4be17532d81062df23cfaa
miguelsbeato <122973208+miguelsbeato@users.noreply.github.com>
2024-05-13 12:59:59 +0200

    Delete docplanner directory

--- docplanner/src/main/java/edu/comillas/icai/gitt/pat/spring/jpa/modelo/RespuestaSala.java
+++
@@ -1,6 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.jpa.modelo;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record RespuestaSala(@NotBlank String nombre, @NotNull Integer prioridad){}
