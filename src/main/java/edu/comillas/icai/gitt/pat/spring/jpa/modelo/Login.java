8a0a7700b8a6d009e060743a0e38afdd0d5d8d31
MariaCastilla05 <122521732+MariaCastilla05@users.noreply.github.com>
2024-05-19 00:51:07 +0200

    Add files via upload

--- src/main/java/edu/comillas/icai/gitt/pat/spring/jpa/modelo/Login.java
+++ src/main/java/edu/comillas/icai/gitt/pat/spring/jpa/modelo/Login.java
@@ -1,5 +1,9 @@
 package edu.comillas.icai.gitt.pat.spring.jpa.modelo;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
-public record Login(@NotBlank String email, @NotBlank String password){}
+public record Login(
+        @Email @NotBlank String email,
+        @NotBlank String password
+) {}
