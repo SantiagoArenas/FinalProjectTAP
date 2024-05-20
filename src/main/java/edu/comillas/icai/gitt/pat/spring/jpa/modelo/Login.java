98a6e752b10dc4e124a0a65d48c8b4fc8def2ada
miguelsbeato <122973208+miguelsbeato@users.noreply.github.com>
2024-05-20 12:18:55 +0200

    Delete src directory

--- src/main/java/edu/comillas/icai/gitt/pat/spring/jpa/modelo/Login.java
+++
@@ -1,9 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.jpa.modelo;
-
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-
-public record Login(
-        @Email @NotBlank String email,
-        @NotBlank String password
-) {}
