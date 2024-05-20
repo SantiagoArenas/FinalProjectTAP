911f9432d66561a72da3615e465e5e884cda6ad4
miguelsbeato <122973208+miguelsbeato@users.noreply.github.com>
2024-05-20 22:44:15 +0200

    Delete src directory

--- src/main/java/edu/comillas/icai/gitt/pat/spring/jpa/modelo/Modificacion.java
+++
@@ -1,6 +0,0 @@
-package edu.comillas.icai.gitt.pat.spring.jpa.modelo;
-
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-
-public record Modificacion(@NotBlank String nombre, @NotBlank String password, @NotNull boolean jefe) {}
